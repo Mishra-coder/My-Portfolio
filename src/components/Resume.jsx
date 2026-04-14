982630df038d0cc4114662190204ea943469951e
Mishra-coder <devendramishra495@gmail.com>
2026-04-15 00:59:35 +0530

    Add Zappify project and update Resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Resume = () => {
-    const resumePdf = "/resume.pdf";
+    const resumePdf = "/DEVENDRA_RESUME.pdf";
 
     return (
         <article className="resume active" data-page="resume">
